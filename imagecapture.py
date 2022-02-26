c5b1a4ff830fa59c0c56705df9b09a2f4f283fbd
pkarkare03 <47032210+pkarkare03@users.noreply.github.com>
2022-02-25 19:19:09 -0500

    Add files via upload

---
+++ imagecapture.py
@@ -0,0 +1,96 @@
+from time import sleep
+from datetime import datetime
+from sh import gphoto2 as gp
+import signal, os, subprocess, os.path
+
+# Kill gphoto process
+
+def killgphoto2Process():
+	p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+	out, err = p.communicate()
+	
+	#search of the gphoto2 process
+	for line in out.splitlines():
+		if b'gvfsd-gphoto2' in line:
+			#kill this process
+			pid = int(line.split(None,1)[0])
+			os.kill(pid, signal.SIGKILL)
+
+# Set date and time for stamping
+			
+shot_date = datetime.now().strftime("%Y-%m-%d")
+picID = "PiShot"
+
+clearCommand = ["--folder", "/store_00020001/DCIM/100CANON", "-R", "--delete-all-files"]
+triggerCommand = ["--trigger-capture"]
+downloadCommand = ["--get-all-files"]
+
+folder_name = shot_date + picID
+save_location = "/media/pi/PIYUSH - SPLM Backup/dslr/Orchid/" + folder_name
+
+def createSaveFolder():
+	try:
+		os.makedirs(save_location)
+	except:
+		print(" ------ > changing to save location", save_location)
+		os.chdir(save_location)
+
+def clear_screen():
+    # It is for MacOS and Linux(here, os.name is 'posix')
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:
+        # It is for Windows platfrom
+        _ = os.system('cls')	
+def captureImages():
+	print ("******* TAKE PIC *********")
+	gp(triggerCommand)
+	sleep(4)
+	gp(downloadCommand)
+	print ("Downloaded image to: ", save_location)
+
+	
+def renameFiles(ID):
+	os.chdir(save_location)
+	global shot_time
+	print ("change Dir: ", save_location)
+	for filename in os.listdir("."):
+		shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		if len(filename) < 13:
+			if filename.endswith(".JPG"):
+				os.rename(filename, (shot_time + ID + ".JPG"))
+				print("JPG renamed:", shot_time)
+			elif filename.endswith(".CR2"):
+				os.rename(filename, (shot_time + ID + ".CR2"))
+				print("RAW renamed")
+
+def convert(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return "%d:%02d:%02d" % (hour, min, sec)
+
+def MyTimer(min,sec):
+	NumSec=min*60+sec
+	NumFiles = len([name for name in os.listdir(save_location) if os.path.isfile(os.path.join(save_location, name))])
+	for s in range(NumSec):	
+		print ("Time remaining(h:m:s): ", convert(NumSec-s))
+		print ("Last Shot          : " + shot_time + picID + ".JPG")
+		print ("Total shots today  : ", NumFiles/2)
+		sleep(1)	
+		clear_screen()
+		
+
+
+# Start the capture
+killgphoto2Process()
+while True:
+	clear_screen()
+	createSaveFolder()
+	captureImages()
+	renameFiles(picID)
+	gp(clearCommand)
+	MyTimer(20,0)
+	
+
+
+
